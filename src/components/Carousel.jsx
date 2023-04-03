c9d5b8f8c49e034f45d70822b926a17efda93838
Sergijus <web.srdjan.marinkovic@gmail.com>
2023-04-03 16:56:05 +0200

    Finished building slider

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import { shortList, list, longList } from '../data';
+import { FaQuoteRight } from 'react-icons/fa';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+const Carousel = () => {
+  const [people, setPeople] = useState(list);
+  const [currentPerson, setCurrentPerson] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length;
+      return result;
+    });
+  };
+
+  useEffect(() => {
+    let sliderId = setInterval(() => {
+      nextSlide();
+    }, 2000);
+    return () => {
+      clearInterval(sliderId);
+    };
+  }, [currentPerson]);
+
+  return (
+    <section className="slider-container">
+      {people.map((person, index) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article
+            className="slide"
+            style={{
+              transform: `translateX(${100 * (index - currentPerson)}%)`,
+              opacity: index === currentPerson ? 1 : 0,
+              visibility: index === currentPerson ? 'visible' : 'hidden',
+            }}
+            key={id}
+          >
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+      <button className="prev" type="button" onClick={prevSlide}>
+        <FiChevronLeft />
+      </button>
+      <button className="next" type="button" onClick={nextSlide}>
+        <FiChevronRight />
+      </button>
+    </section>
+  );
+};
+export default Carousel;
